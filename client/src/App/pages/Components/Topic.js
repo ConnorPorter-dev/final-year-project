b27b599d38cad3256390f517e28a804306d7bf00
ConnorPorter-dev <52919851+ConnorPorter-dev@users.noreply.github.com>
2020-12-04 11:47:32 +0000

    Added Topic Page

--- client/src/App/pages/Components/Topic.js
+++ client/src/App/pages/Components/Topic.js
@@ -1,17 +1,39 @@
-import React, { useState } from 'react';
-function Topic(props) {
-    return (
-        <div>
-            <h1>{props.name}</h1>
-            <p>{props.description}</p>
+import React, { useState, useEffect } from 'react';
+
+// Component to display a topic information on the page based on the url
+function Topic({ match }) {
+
+    const [loading, setLoading] = useState(true)
+    const [topic, setTopic] = useState({})
+
+    useEffect(() => {
+        getTopic();
+    }, []);
+
+    const getTopic = () => {
+        fetch(`/api/topic/${match.params.topicID}`)
+            .then(res => res.json())
+            .then(topic => setTopic(topic))
+            .then(x => setLoading(false))
+    }
+
+    if (!loading) {
+        return (
             <div>
-                {props.video.embed
-                    ? <iframe src={props.video.link} width="560" height="315"></iframe>
-                    : <a href={props.video.link}>Youtube Link</a>
-                }
+                <h1>{topic.name}</h1>
+                <p>{topic.description}</p>
+                <div>
+                    {topic.video.embed
+                        ? <iframe src={topic.video.link} width="560" height="315"></iframe>
+                        : <a href={topic.video.link}>Video Link</a>
+                    }
+                </div>
             </div>
-        </div>
-    );
+        )
+    } else {
+        return <h1>Loading...</h1>
+    }
+
 }
 
 export default Topic
