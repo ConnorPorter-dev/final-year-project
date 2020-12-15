24c8ac2ef07973a1df9f501a416383a859f48283
ConnorPorter-dev <52919851+ConnorPorter-dev@users.noreply.github.com>
2020-12-15 17:51:47 +0000

    Custom lines needed for onclick event

--- client/src/App/pages/Components/CustomCode/index.js
+++ client/src/App/pages/Components/CustomCode/index.js
@@ -4,12 +4,22 @@ const CustomCode = (props) => {
 
     return (
         <div>
-            { props.lines.map(line => <p className="line">{line}</p>) }
+            { props.lines.map(line => <p className="line">{line}</p>)}
         </div>
     )
 }
 // Need to have an onclick on every line
 // Could do this with a new component- pass onclick info to this for every line
 
+const codeLine = (props) => {
+    const clicked = () => {
+        // When clicked, state should change in customCode to display links to line analysis
+        // Or - Change input on invisible box using CSS
+        // Eg- Edit text by CSS ID and then make it visible
+    }
+    return (
+        <p className="line">{props.line}</p>)
+    )
+}
 
 export default CustomCode
