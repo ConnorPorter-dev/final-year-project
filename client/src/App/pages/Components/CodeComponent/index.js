96432a3e4a3e684beb434107e34ee3e422fb891a
ConnorPorter-dev <52919851+ConnorPorter-dev@users.noreply.github.com>
2020-12-20 18:12:19 +0000

    Refactored and enhanced CodeComponent for new DS

--- client/src/App/pages/Components/CodeComponent/index.js
+++ client/src/App/pages/Components/CodeComponent/index.js
@@ -1,15 +1,26 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-// SyntaxHighlighter.registerLanguage('java', java);
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
+// CodeComponent will display the code on the page along with the option to show an analysis of each line of code, selected by a drop down menu
 const CodeComponent = (props) => {
+    const [selectedLine, setLine] = useState(null) // Selected line is currently displayed analysed line
 
     return (
         <div>
             <SyntaxHighlighter language="java" style={dracula} showLineNumbers={true} wrapLongLines={true}>
-                {props.code}
+                {props.content.fullcode}
             </SyntaxHighlighter>
-            <div>Line Analysis: {props.topics.map(item => item + " ")}</div>
+            <select onChange={e => setLine(props.content.lines.find(line => e.target.value == line.linenumber))}>
+                <option value={null} selected></option>
+                {props.content.lines.map(line => <option key={line.linenumber} value={line.linenumber}>Line {line.linenumber}</option>)}
+            </select>
+            {
+                (selectedLine != null) 
+                ? selectedLine.links.map(topic => <Link to={`/topic/${topic.id}`}>{topic.name}</Link>)
+                : <></>
+            }
         </div>
 
     )
