2c6b2d33158d47859c7a293714e5aa4a64debed7
ConnorPorter-dev <52919851+ConnorPorter-dev@users.noreply.github.com>
2021-01-06 18:27:26 +0000

    Auto regex analysis of pre-seperated lines

--- index.js
+++ index.js
@@ -34,9 +34,41 @@ app.listen(port);
 
 console.log('App is listening on port ' + port);
 
-// Very costly function - should be used sparingly
+// Very costly function for larger data sets- should be used sparingly
 // TODO: design conditions that this will trigger 
+// TODO: Figure out O notation
 const updateLines = () => {
+    const newRegArr = () => {
+        let arr = []
+        topicData.map(topic => {
+            if (topic.regexkey.length > 0) {
+                arr.push({
+                    "regexkey": topic.regexkey,
+                    "name": topic.name,
+                    "id": topic.id
+                })
+            }
+        })
+        return arr
+    }
+    const regexArray = newRegArr()
+
+    const createLinks = (line) => {
+        line.links = []
+        regexArray.map(topic => {
+            topic.regexkey.map(regexStr => {
+                exp = new RegExp(regexStr)
+                if (exp.test(line.line)) {
+                    line.links.push({
+                        "name": topic.name,
+                        "id": topic.id
+                    })
+                }
+            })
+        })
+        return line
+    }
+
     // Cycle through loaded data:
     // Every Topic
     // Every Content in Topic where type=code
@@ -46,14 +78,19 @@ const updateLines = () => {
             if (content.type != "code") {
                 return content
             }
-            content.lines.map(line => {
+            let newLines = content.lines.map(line => {
                 console.log(line.line);
-                // Pass every line to new function that processes line and then returns with new line that includes 
-                return content
+                line = createLinks(line)
+                return line
+                // Pass every line to new function that processes line and then returns with new line that includes new line.links
             })
+            console.log(newLines);
+            return content
         })
         return topic
     })
+
+    
 
     //Save Data function
 }
