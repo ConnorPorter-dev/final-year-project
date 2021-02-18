017c0f694e1769584710faf0a6b3651fea6ae4d3
ConnorPorter-dev <52919851+ConnorPorter-dev@users.noreply.github.com>
2021-02-18 11:45:58 +0000

    Started changing landing page

--- client/src/App/pages/Home.js
+++ client/src/App/pages/Home.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { MDBBtn } from "mdbreact";
 
-
-class Home extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Project Home</h1>
-        {/* Link to List.js */}
-        <Link to={'./test'}>
-          <button variant="raised">
-            TestData
-        </button>
-        </Link>
-        <Link to={'./test'}>
-          <button variant="raised">
-            Basic Pagination
-        </button>
-        </Link>
-      </div>
-    );
-  }
+const Home = () => {
+    return (<div className="App">
+    <h1>Code Port</h1>
+    {/* <MDBBtn color="primary">Primary</MDBBtn> */}
+    {/* Link to List.js */}
+    <Link to={'./test'}>
+      <button variant="raised">
+        Start your journey!
+    </button>
+    
+    </Link>
+    {/* <MDBBtn gradient="purple">Test</MDBBtn> */}
+    <Link to={'./test'}>
+      <button variant="raised">
+        Basic Pagination
+    </button>
+    </Link>
+  </div>)
 }
+
+// class Home extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Code Port</h1>
+//         <MDBBtn color="primary">Primary</MDBBtn>
+//         {/* Link to List.js */}
+//         <Link to={'./test'}>
+//           <button variant="raised">
+//             Start your journey!
+//         </button>
+        
+//         </Link>
+//         {/* <MDBBtn gradient="purple">Test</MDBBtn> */}
+//         <Link to={'./test'}>
+//           <button variant="raised">
+//             Basic Pagination
+//         </button>
+//         </Link>
+//       </div>
+//     );
+//   }
+// }
 export default Home;
