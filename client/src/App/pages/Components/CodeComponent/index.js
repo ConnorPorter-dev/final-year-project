d6dc69bd6830c175e0ae130ffebd99d0e97a99fb
ConnorPorter-dev <52919851+ConnorPorter-dev@users.noreply.github.com>
2021-02-22 23:30:35 +0000

    Revamped code component to have popup links

--- client/src/App/pages/Components/CodeComponent/index.js
+++ client/src/App/pages/Components/CodeComponent/index.js
@@ -8,31 +8,58 @@ import { Link } from 'react-router-dom';
 const CodeComponent = (props) => {
     const [selectedLine, setLine] = useState(null) // Selected line is currently displayed analysed line
 
+    const LineClicked = () => {
+        return (
+            <div className="link-box">
+                {
+                    (selectedLine != null)
+                        ? <div className="row">
+                            <div className="col s12 m6">
+                                <div className="card blue-grey darken-1 z-depth-3">
+                                    <div className="card-content white-text">
+                                        <span className="card-title">Smart Analysis</span>
+                                        <p>Line {selectedLine.linenumber}</p>
+                                    </div>
+                                    <div className="card-action">
+                                        <div>                                        
+                                            {selectedLine.links.map(topic => <Link key={topic.id} to={`/topic/${topic.id}`}><button className="topic-button">{topic.name}</button></Link>)}
+                                        </div>
+                                        <button className="close-button" onClick={e => setLine(null)}>Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        : <></>
+                }
+            </div>
+
+        )
+    }
+
+
     return (
         <div className="code-component">
-            <SyntaxHighlighter language="java" style={dracula} showLineNumbers={true} wrapLongLines={true}>
-                {props.content.data}
-            </SyntaxHighlighter>
-            <div className="analysis-container">
-                <h2 className="analysis-title">Line Analysis</h2>
-                <select onChange={e => setLine(props.content.lines.find(line => e.target.value == line.linenumber))}>
-                    <option value={null} selected></option>
-                    {props.content.lines.map(line => <option key={line.linenumber} value={line.linenumber}>Line {line.linenumber + 1}</option>)}
-                </select>
-                {/* {
-                (selectedLine != null) 
-                ? selectedLine.links.map(topic => <Link to={`/topic/${topic.id}`}>{topic.name}</Link>)
-                : <></>
-            } */}
-                <div>
-                    {
-                        (selectedLine != null)
-                            ? selectedLine.links.map(topic => <Link key={topic.id} to={`/topic/${topic.id}`}><button className="topic-button">{topic.name}</button></Link>)
-                            : <></>
-                    }
-                </div>
+            <LineClicked />
+            <div className="row">
+                <div className="col s1 link-container">
+                    <ul>
+                        {
+                            props.content.lines.map(line =>
+                                <li className="line-link right-align" key={line.linenumber} value={line.linenumber} onClick={e => setLine(props.content.lines.find(line => e.target.getAttribute("value") == line.linenumber))}>
+                                    Line {line.linenumber + 1}
+                                </li>
+                            )
 
+                        }
+                    </ul>
+                </div>
+                <div className="code-box col s11">
+                    <SyntaxHighlighter language="java" style={dracula} showLineNumbers={false} wrapLongLines={false}>
+                        {props.content.data}
+                    </SyntaxHighlighter>
+                </div>
             </div>
+
         </div>
 
     )
